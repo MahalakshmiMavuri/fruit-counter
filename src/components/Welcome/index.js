3ff12240fa42b30724790d7146670dcc1dda20a2
mahalakshmi <mahalakshmimavuri2@gmail.com>
2023-05-07 22:37:47 +0000

    fruit-counter

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class WelcomeApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+      </div>
+    )
+  }
+}
+export default WelcomeApp
